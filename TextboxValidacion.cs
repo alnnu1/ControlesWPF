b5d57b6d58ba0efe585bc485686a4e52ae267893
alnn0115 <alnn0115@gmail.com>
2025-01-02 01:14:36 -0600

    Add project files.

---
+++ TextboxValidacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+/// <summary>
+/// Controles con propiedades de validación y de base de datos
+/// </summary>
+namespace ControlesWPF
+{
+    /// <summary>
+    /// Textbox con propiedades de validación
+    /// </summary>
+    public class TextboxValidacion : TextBox
+    {
+        #region Propiedades
+
+        /// <summary>
+        /// Elimina los espacios del principio y al final del Textbox
+        /// </summary>
+        public bool EliminarEspacios { get; set; }
+
+        #endregion
+
+        #region Eventos
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+
+            if (EliminarEspacios)
+            {
+                Text = Text.Trim();
+            }
+        }
+
+        #endregion
+    }
+}
